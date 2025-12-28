0f770db7ab7a8a6781aabb0157e3cdb25c744ade
Shihtzu299 <shihtzu299@gmail.com>
2025-12-28 16:18:24 +0100

    updatez

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rukqs99q7l8"
+    "revision": "0.o21iv2h26po"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
